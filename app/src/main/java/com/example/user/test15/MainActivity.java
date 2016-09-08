2ca739b15a3b701e4c518570ea78ef5df74847c1
Tree <Tree>
2016-09-08 23:19:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/test15/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.test15;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt = (Button) findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:146987432847"));
+                startActivity(intent);
+            }
+        });
+    }
+}
